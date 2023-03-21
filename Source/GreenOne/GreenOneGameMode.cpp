36c3a67e42388890ac28c44f3858b952da1817e2
DevOfSomewhere <68583362+SunvyWasTaken@users.noreply.github.com>
2023-03-21 13:07:28 +0100

    Camera plus truc game mode (#6)

--- Source/GreenOne/GreenOneGameMode.cpp
+++ Source/GreenOne/GreenOneGameMode.cpp
@@ -7,7 +7,7 @@
 AGreenOneGameMode::AGreenOneGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/GreenOne/Blueprints/BP_Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
