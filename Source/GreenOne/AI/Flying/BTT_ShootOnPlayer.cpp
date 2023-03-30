213442ae4ab0521ffe63a9d0daf24b7fbba593d5
DevOfSomewhere <keanu.vongue@gmail.com>
2023-03-30 10:31:16 +0200

    Better Flying again.
    Paramétrage du charcter movement pour que le flying soit mieux et aussi avoir un meilleur resultat.

--- Source/GreenOne/AI/Flying/BTT_ShootOnPlayer.cpp
+++ Source/GreenOne/AI/Flying/BTT_ShootOnPlayer.cpp
@@ -10,13 +10,21 @@ UBTT_ShootOnPlayer::UBTT_ShootOnPlayer()
 	bCreateNodeInstance = true;
 }
 
+//This function is used to execute the task ShootOnPlayer.
+//It checks if the owner of the task is a child of AFlyingAICharacter.
+//If it is, it calls the Shoot() function and returns a Succeeded result. Otherwise, it logs a warning and returns a Failed result. 
 EBTNodeResult::Type UBTT_ShootOnPlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	//Check if the owner of the task is a child of AFlyingAICharacter
 	if (AFlyingAICharacter* PawnRef = Cast<AFlyingAICharacter>(OwnerComp.GetAIOwner()->GetPawn()))
 	{
+		//Call the Shoot() function
 		PawnRef->Shoot();
+		//Return a Succeeded result
 		return EBTNodeResult::Succeeded;
 	}
+	//Log a warning
 	UE_LOG(LogTemp, Warning, TEXT("Le owner du task ShootOnPlayer n'est pas a child de AFlyingAICharacter."));
+	//Return a Failed result
 	return EBTNodeResult::Failed;
 }
