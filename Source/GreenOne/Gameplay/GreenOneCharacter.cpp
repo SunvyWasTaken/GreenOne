79d852394cdd0db8bcaebbe29e621bc32a732028
DevOfSomewhere <keanu.vongue@gmail.com>
2023-04-19 09:49:57 +0200

    Debut AnimBP

--- Source/GreenOne/Gameplay/GreenOneCharacter.cpp
+++ Source/GreenOne/Gameplay/GreenOneCharacter.cpp
@@ -92,7 +92,7 @@ AGreenOneCharacter::AGreenOneCharacter(const FObjectInitializer& ObjectInitializ
 	
 	// Add TargetMuzzle
 	TargetMuzzle = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleTarget"));
-	TargetMuzzle->SetupAttachment(GetMesh());
+	TargetMuzzle->SetupAttachment(GetMesh(), FName("hand_rSocket"));
 	
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
