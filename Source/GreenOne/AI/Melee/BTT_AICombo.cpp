9619cb867eeb4e4d3500547d36c71444f858c8a5
idryssj31 <55395114+idryssj31@users.noreply.github.com>
2023-04-26 10:34:10 +0200

    Change V1

---
+++ Source/GreenOne/AI/Melee/BTT_AICombo.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GreenOne/AI/Melee/BTT_AICombo.h"
+
---
+++ Source/GreenOne/AI/Melee/BTT_AICombo.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTT_AICombo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GREENONE_API UBTT_AICombo : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
