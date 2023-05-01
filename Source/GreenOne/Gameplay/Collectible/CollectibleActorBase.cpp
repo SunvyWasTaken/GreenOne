0bec723b3899c01b29055827d93aa8e63ced73d9
NysoS <k.ledoux.dev@gmail.com>
2023-05-01 04:31:56 +0200

    detect collector

--- Source/GreenOne/Gameplay/Collectible/CollectibleActorBase.cpp
+++ Source/GreenOne/Gameplay/Collectible/CollectibleActorBase.cpp
@@ -2,54 +2,67 @@
 
 
 #include "CollectibleActorBase.h"
+
+#include "CollectorInterface.h"
 #include "Components/BoxComponent.h"
 
 // Sets default values
 ACollectibleActorBase::ACollectibleActorBase()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
 	RootComponent = MeshComponent;
+
+	ShapeComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Detection Shape"));
+	ShapeComponent->SetGenerateOverlapEvents(true);
+	ShapeComponent->SetupAttachment(RootComponent);
 }
 
 void ACollectibleActorBase::PostCDOCompiled()
 {
 	Super::PostCDOCompiled();
-	if(TypeShapeComponent)
-	{
-		if (UShapeComponent* ShapeExist = FindComponentByClass<UShapeComponent>())
-		{
-			RemoveInstanceComponent(ShapeExist);
-			InitDynamicCollisionShape();
-		}else
-		{
-			InitDynamicCollisionShape();
-		}
-	}
+	// if(TypeShapeComponent)
+	// {
+	// 	if (UShapeComponent* ShapeExist = FindComponentByClass<UShapeComponent>())
+	// 	{
+	// 		RemoveInstanceComponent(ShapeExist);
+	// 		InitDynamicCollisionShape();
+	// 	}else
+	// 	{
+	// 		InitDynamicCollisionShape();
+	// 	}
+	// }
 }
 
 // Called when the game starts or when spawned
 void ACollectibleActorBase::BeginPlay()
 {
 	Super::BeginPlay();
+	if(ShapeComponent)
+	{
+		ShapeComponent->OnComponentBeginOverlap.AddUniqueDynamic(this, &ACollectibleActorBase::OnCollectibleActorBeginOverlap);
+	}
 }
 
 void ACollectibleActorBase::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
 	Super::EndPlay(EndPlayReason);
+	if(ShapeComponent)
+	{
+		ShapeComponent->OnComponentBeginOverlap.RemoveDynamic(this, &ACollectibleActorBase::OnCollectibleActorBeginOverlap);
+	}
 }
 
 void ACollectibleActorBase::OnCollectibleActorBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                                                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
                                                            bool bFromSweep, const FHitResult& SweepResult)
 {
-}
-
-void ACollectibleActorBase::OnCollectibleActorEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-                                                         UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
+	UE_LOG(LogTemp, Warning, TEXT("B Collectible"));
+	if(!OtherActor || !Cast<ICollectorInterface>(OtherActor)) return;
+	
+	UE_LOG(LogTemp, Warning, TEXT("Take Collectible"));
 }
 
 // Called every frame
@@ -58,10 +71,16 @@ void ACollectibleActorBase::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+void ACollectibleActorBase::Action(AActor* Actor)
+{
+	ICollectibleInterface::Action(Actor);
+}
+
 void ACollectibleActorBase::InitDynamicCollisionShape()
 {
 	ShapeComponent = NewObject<UShapeComponent>(this, TypeShapeComponent);
-	ShapeComponent->SetupAttachment(RootComponent);
+	ShapeComponent->SetupAttachment(MeshComponent);
+	ShapeComponent->SetGenerateOverlapEvents(true);
 	ShapeComponent->bAutoRegister = true;
 	AddInstanceComponent(ShapeComponent);
 	TypeShapeComponent = nullptr;
